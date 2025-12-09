96103ecca6b01a8b6daea0537e3d4212b858c05b
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-12-09 23:25:31 +0000

    I give up tonight

--- Godot/Entities/User Interface/GD_MainMenu.gd
+++ Godot/Entities/User Interface/GD_MainMenu.gd
@@ -1,19 +1,23 @@
-extends Node3D
+extends Node
 
-signal SIGNAL_Start
 
 @export var UI_AudioStreamPlayer_ref : AudioStreamPlayer
-@onready var environment:Environment = $"WorldEnvironment".environment
-var exit_sound = load("res://Audio/good-game.ogg")
+@export var world_environment : WorldEnvironment
+@export var exit_sound : AudioStream
 
 func _on_btn_exit_pressed() -> void:
+    if UI_AudioStreamPlayer_ref == null:
+        UI_AudioStreamPlayer_ref = $"../../../Player/UI_AudioPlayer"
     UI_AudioStreamPlayer_ref.stream = exit_sound
     UI_AudioStreamPlayer_ref.play(0)
     var tween = create_tween()
-    tween.tween_property(environment , "background_energy_multiplier", 0.0, 2.0)
+    if world_environment == null:
+        world_environment = $"../../../WorldEnvironment"
+    tween.tween_property(world_environment.environment , "background_energy_multiplier", 0.0, 2.0)
+  
     await get_tree().create_timer(3).timeout
     get_tree().quit(0)
 
 
 func _on_btn_start_pressed() -> void:
-    SIGNAL_Start.emit() 
+    $"../../../All Seeds"._on_start_select()
--- Godot/Entities/User Interface/Seeds/GD_SeedSelection.gd
+++ Godot/Entities/User Interface/Seeds/GD_SeedSelection.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-@export var StartMenu_ref : XRToolsViewport2DIn3D
+@export var StartMenu_ref : Node
 
 func _ready() -> void:
-    StartMenu_ref.SIGNAL_Start.connect(_on_start_select)
+    #StartMenu_ref.SIGNAL_Start.connect(_on_start_select)
     visible = false
+    pass
     
 func _on_start_select():
     visible = true
