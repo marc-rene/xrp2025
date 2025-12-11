5b5e8261e0e9c4b45fcb5002ed3a06aee33ec416
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-12-11 17:41:06 +0000

    Added loads debugging to Plough

--- Godot/Entities/plough.gd
+++ Godot/Entities/plough.gd
@@ -3,6 +3,7 @@ extends Node3D
 signal plough_dig(world_pos: Vector3)
 
 @onready var blade_area: Area3D = $PickableObject/Blade
+@onready var pickable := $PickableObject
 
 var is_held := false
 var touching_soil := false
@@ -10,21 +11,40 @@ var last_pos := Vector3.ZERO
 
 
 func _ready():
-	# Connect blade collisions
+	# Connect XRTools pick-up signals
+	pickable.picked_up.connect(_on_picked_up)
+	pickable.released.connect(_on_released)
+
+	# Connect blade collision signals
 	blade_area.body_entered.connect(_on_blade_enter)
 	blade_area.body_exited.connect(_on_blade_exit)
-	pass
-	
+
+	print("Plough ready. Waiting for pickup.")
+
+
+# XRTools: object is grabbed
+func _on_picked_up(by):
+	is_held = true
+	last_pos = global_transform.origin
+	print("Plough picked up")
+
+
+# XRTools: object released
+func _on_released(by):
+	is_held = false
+	print("Plough released")
+
+
 func _on_blade_enter(body):
 	if body.is_in_group("soil"):
 		touching_soil = true
-		print("Blade touching soil")
+		print("Blade touching soil!")
 
 
 func _on_blade_exit(body):
 	if body.is_in_group("soil"):
 		touching_soil = false
-		print("Blade left soil")
+		print("Blade left soil!")
 
 
 func _physics_process(delta):
@@ -33,14 +53,12 @@ func _physics_process(delta):
 	if not touching_soil:
 		return
 
-	# Detect dragging motion
 	var current = global_transform.origin
-	var movement = current.distance_to(last_pos)
+	var moved := current.distance_to(last_pos)
 
-	if movement > 0.01:
-		# Emit world pos at blade contact point
+	if moved > 0.01:
 		var contact_pos = blade_area.global_transform.origin
-		emit_signal("plough_dig", contact_pos)
 		print("DIG:", contact_pos)
+		emit_signal("plough_dig", contact_pos)
 
 	last_pos = current
