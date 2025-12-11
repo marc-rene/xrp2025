777fa30fdf0437d0f41911086b5b9744f6df3db7
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-12-11 18:02:33 +0000

    Option to skip to watering

--- Godot/Entities/Growing a Seed/GD_Potatoe_Growth.gd
+++ Godot/Entities/Growing a Seed/GD_Potatoe_Growth.gd
@@ -15,15 +15,30 @@ func _ready():
 func _update_speed_scale(new_speed):
 	$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale = new_speed
 
-
+var keep_going = false
 func _start_growing():
 	$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "Scene"
 	_update_speed_scale(Speed_Scale)
 	for day in range(90):
-		$Label3D.text = "Day: %d" % day
-		await get_tree().create_timer(0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale).timeout
-		if day == 80:
-			$"New Tatters".scale(0.01)
-			$"New Tatters".visible = false
-			var growy_tween = create_tween()
-			growy_tween.tween_property($"New Tatters", "scale", 1, (0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale)* 20).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		if keep_going == false:
+			day -= 1
+		else:
+			$Label3D.text = "Day: %d" % day
+			await get_tree().create_timer(0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale).timeout
+			if day == 80:
+				$"New Tatters".scale(0.01)
+				$"New Tatters".visible = false
+				var growy_tween = create_tween()
+				growy_tween.tween_property($"New Tatters", "scale", 1, (0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale)* 20).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("HEY ", body.name, " JUST ENTERED")
+	if body.name == "PickupableWaterCan":
+		keep_going = true
+		_start_growing()
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.name == "PickupableWaterCan":
+		keep_going = false
