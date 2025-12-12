e73e4d9ee58c9f12aefc4435865f8a895ebf413a
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-12-12 02:12:56 +0000

    Cooking potatoes with water
    partcile system and alignment work

--- Godot/Entities/Growing a Seed/GD_Potatoe_Growth.gd
+++ Godot/Entities/Growing a Seed/GD_Potatoe_Growth.gd
@@ -5,66 +5,68 @@ extends Node
 # 10 days pass for every 0.41625 seconds of play
 # 24 days pass for every second of play
 @export var Speed_Scale = 0.1
+@export var Water_Can_Ref : Node3D
 
 var start_spawn_of_new_potatoes = false
 
 func _ready():
-	$"New Tatters".visible = false
-	$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "[stop]"
-	#$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position = 0
+    $"New Tatters".visible = false
+    $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "[stop]"
+    #$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position = 0
 
 func _update_speed_scale(new_speed):
-	$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale = new_speed
-	print("Growing Speed is now ", new_speed)
+    $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale = new_speed
+    print("Growing Speed is now ", new_speed)
 
 var keep_going = false
 func _start_growing():
-	print("STARTED GROWING MASHALLAH")
-	$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "Scene"
-	#$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position = 0
-	_update_speed_scale(Speed_Scale)
-	for day in range(90):
-		print("On day: ", day)
-		if keep_going == false:
-			day -= 1
-			print("Keep going is false, day is now: ", day)
-		else:
-			print("Text should be : 'Day: %d'" % day)
-			$Label3D.text = "Day: %d" % day
-			await get_tree().create_timer(0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale).timeout
-			print("Timer finished")
-			if day == 80:
-				$"New Tatters".scale(0.01)
-				$"New Tatters".visible = false
-				var growy_tween = create_tween()
-				growy_tween.tween_property($"New Tatters", "scale", 1, (0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale)* 20).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-
+    print("STARTED GROWING MASHALLAH")
+    $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "Scene"
+    #$Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position = 0
+    _update_speed_scale(Speed_Scale)
+    for day in range(90):
+        print("On day: ", day)
+        $Label3D.text = "Day: %d" % day
+        await get_tree().create_timer(0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale).timeout
+        print("Timer finished")
+        if day == 80:
+            print("Day was 80, starting new tween for new spuds")
+            $"New Tatters".scale = Vector3(0.01, 0.01, 0.01)
+            $"New Tatters".visible = true
+            var growy_tween = create_tween()
+            growy_tween.tween_property($"New Tatters", "scale", Vector3.ONE, (0.041625 / $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.speed_scale)* 20).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+    $Label3D.text = "TAYTO FOR EVERYONE"
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	#print("HEY ", body.name, " JUST ENTERED")
-	if body.name == "PickupableWaterCan":
-		print("Should Start to grow")
-		keep_going = true
-		_start_growing()
+    #print("HEY ", body.name, " JUST ENTERED")
+    if body.name == "PickupableWaterCan":
+        print("Should Start to grow")
+        keep_going = true
+        _start_growing()
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	if body.name == "PickupableWaterCan":
-		print("Should STOP to grow")
-		keep_going = false
+    if body.name == "PickupableWaterCan":
+        print("Should STOP to grow")
+        keep_going = false
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if area.name == "WATER_CAN_END" or area.name == "PickupableWaterCan":
-		print("Should START to grow")
-		if $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position <= 0.01:
-			_start_growing()
-			keep_going = true
-		else:
-			keep_going = true
+    if area.name == "WATER_CAN_END" or area.name == "PickupableWaterCan":
+        print("Should START to grow")
+        if Water_Can_Ref.facing_down == true:
+            print("Can's facing down too yay!")
+        else: 
+            print("Want to start growing but need to water")
+            return
+        if $Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation_position <= 0.01:
+            _start_growing()
+            keep_going = true
+        else:
+            keep_going = true
 
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
-	if area.name == "WATER_CAN_END" or area.name == "PickupableWaterCan":
-		print("Should STOP to grow")
-		keep_going = false 
+    if area.name == "WATER_CAN_END" or area.name == "PickupableWaterCan":
+        print("Should STOP to grow")
+        keep_going = false 
---
+++ Godot/Entities/Growing a Seed/GD_Water_Can.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@export var facing_down = false
+
+func _ready() -> void:
+    if facing_down == false:
+        print("Starting off the sprinklers should be off!")
+        $PickableObject/WATER_CAN_END/GPUParticles3D.emitting = false
+
+func _physics_process(delta: float) -> void:
+    var end: Node3D = $PickableObject/WATER_CAN_END
+    if end == null:
+        return 
+
+    var forward: Vector3 = end.global_transform.basis.x.normalized()
+    var down: Vector3 = Vector3.DOWN
+    var alignment := forward.dot(down)
+
+    # How much do be "mostly down" (e.g > cos(45d) ~ 0.707)
+    if alignment > 0.7:
+        facing_down = true
+        $PickableObject/WATER_CAN_END/GPUParticles3D.emitting = true
+        if $PickableObject/AudioStreamPlayer3D.playing == false:
+            print("LLET IT RAIN")
+            $PickableObject/AudioStreamPlayer3D.play()
+    else:
+        print("No more rain")
+        $PickableObject/AudioStreamPlayer3D.playing = false
+        $PickableObject/WATER_CAN_END/GPUParticles3D.emitting = false
+        facing_down = false
+        
+    if alignment > 0.95:
+        $PickableObject/WATER_CAN_END/GPUParticles3D.amount = 120
+    elif alignment > 0.85:
+        $PickableObject/WATER_CAN_END/GPUParticles3D.amount = 100
+    elif alignment > 0.75:
+        $PickableObject/WATER_CAN_END/GPUParticles3D.amount = 90
+    elif alignment > 0.7:
+        $PickableObject/WATER_CAN_END/GPUParticles3D.amount = 80
