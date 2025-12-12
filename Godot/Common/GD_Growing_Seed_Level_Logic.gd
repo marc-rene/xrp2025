12ebc4410961bde3d90fe0c12fe15bb1caee2ea5
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-12-12 02:43:32 +0000

    Hand pos for Plough grab changed and added audio effect into file

--- Godot/Common/GD_Growing_Seed_Level_Logic.gd
+++ Godot/Common/GD_Growing_Seed_Level_Logic.gd
@@ -3,28 +3,28 @@ extends Node3D
 const POTATO_SCENE : PackedScene = preload("res://Entities/Growing a Seed/Potato_seed_Growing.tscn")
 
 func _ready() -> void:
-    if GlobalVariables.SEED_PLANT_LOCATIONS == null or GlobalVariables.SEED_PLANT_LOCATIONS.size() <= 0:
-        #print("CRAP! Never planted anything... assuming 3,0,0 for now")
-        #GlobalVariables.SEED_PLANT_LOCATIONS.push_front(Vector3(3,0,0))
-        pass
-        
-    if GlobalVariables.SELECTED_SEED == "POTATO" or GlobalVariables.SELECTED_SEED == null:
-        print("Using potato growing")
-        #for spuddy_idx in range(GlobalVariables.SEED_PLANT_LOCATIONS.size()):
-        var spud = $PotatoSeedGrowing
-        $PotatoSeedGrowing/Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "Scene"
-        $PotatoSeedGrowing/Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.pause()
-        
-        spud.scale = Vector3.ZERO
-        var tween = create_tween()
-        var overshoot_scale = Vector3(1.2,1.5,1.2)
-        tween.set_trans(Tween.TRANS_ELASTIC)
-        tween.set_ease(Tween.EASE_OUT)
-        tween.tween_property(spud, "scale", overshoot_scale, 0.25)
-        print("Creating spud timer")
-        await get_tree().create_timer(0.25).timeout
-        tween.tween_property(spud, "scale", Vector3.ONE, 0.15)
-        print("Spud should be fully spawned by now")
-        
+	if GlobalVariables.SEED_PLANT_LOCATIONS == null or GlobalVariables.SEED_PLANT_LOCATIONS.size() <= 0:
+		#print("CRAP! Never planted anything... assuming 3,0,0 for now")
+		#GlobalVariables.SEED_PLANT_LOCATIONS.push_front(Vector3(3,0,0))
+		pass
+		
+	if GlobalVariables.SELECTED_SEED == "POTATO" or GlobalVariables.SELECTED_SEED == null:
+		print("Using potato growing")
+		#for spuddy_idx in range(GlobalVariables.SEED_PLANT_LOCATIONS.size()):
+		var spud = $PotatoSeedGrowing
+		$PotatoSeedGrowing/Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.current_animation = "Scene"
+		$PotatoSeedGrowing/Potatoe_WORK_CURSE_YOU_DAMN/AnimationPlayer.pause()
+		
+		spud.scale = Vector3.ZERO
+		var tween = create_tween()
+		var overshoot_scale = Vector3(1.2,1.5,1.2)
+		tween.set_trans(Tween.TRANS_ELASTIC)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(spud, "scale", overshoot_scale, 0.25)
+		print("Creating spud timer")
+		await get_tree().create_timer(0.25).timeout
+		tween.tween_property(spud, "scale", Vector3.ONE, 0.15)
+		print("Spud should be fully spawned by now")
+		
 func _physics_process(delta: float) -> void:
-    pass
+	pass
